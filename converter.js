cdf7a3f2560610593207fc1fdf0a5b0b2382548c
Shiluli <cs.adulu@gmail.com>
2022-11-07 15:47:55 -0800

    js

---
+++ converter.js
@@ -0,0 +1,7 @@
+function currency(){
+    let dollars = 145;
+    let ksh = 1000;
+    let toKsh = dollars * ksh;
+    console.log(`145 dollars is ${toKsh} ksh`);
+}
+currency();
---
+++ even.js
@@ -0,0 +1,10 @@
+function even(){
+    let nums = 40;
+    if (nums % 2 === 0){
+        console.log('number is even');
+    }
+    else{
+        console.log('number is odd');
+    }
+};
+even();
